db3861717a3b5457e6319aa6c4009e41e5c6be3c
Eston Kagwima <ekagwima745@gmail.com>
2021-03-06 21:12:28 +0300

    adding constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
   $("#myBtn").click(function(){
-    var selectedRadio = $('input[name="inlineRadioOptions"]:checked').val();
+   var selectedRadio = $('input[name="inlineRadioOptions"]:checked').val();
     if (selectedRadio==="delivery"){
-    $("#myModal").modal();
-  }
+       $("#myModal").modal();
+    }
+    else {
+      alert("your order will be processed shortly")
+    };
+    
   });
   $("#checkout").click(function(){
     var deliveryAddress = $("input#inputAddress").val();
-    alert(' your order will be delivered to ' + deliveryAddress);
+    var quantity = $("input#qty").val();
+    alert(' your order of '+ quantity +' Pizzas'+ ' will be delivered to '+ deliveryAddress);
 
   });
 
